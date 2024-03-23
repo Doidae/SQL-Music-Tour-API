0948a5524e0c7b84241d1d40f6602a7874e51062
Doidae <andyarredondo1224@gmail.com>
2024-03-23 14:04:15 -0400

    Added other routes

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -11,4 +11,40 @@ bands.get('/', async(req, res) => {
     
 })
 
+bands.post('/', async (req, res) =>{
+    try{
+        const newBand = await Band.create(req.body)
+        res.json(newBand)
+    } catch (e) {
+        res.send(e.message)
+    }
+})
+
+
+bands.put('/:id', async (req, res) => {
+    try {
+        const { name, genre } = req.body
+        if (!name && !genre) {
+            throw Error('No fields to update')
+        }
+        const updatedBand = await Band.update(req.body, {
+            where: { id: req.params.id }
+        })
+        res.json(updatedBand)
+    } catch (e) {
+        res.send(e.message)
+    }
+})
+
+bands.delete('/:id', async (req, res) =>{
+    try {
+        const deleted = await Band.destroy({
+            where: { id: req.params.id }
+        })
+        res.send(`Deleted ${deleted} bands.`)
+    } catch (e) {
+        res.send(e.message)
+    }
+})
+
 module.exports = bands
